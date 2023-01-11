028c8d5f3bb967aa5f9d1f73bfd1c43bfb196240
Sakshi Singh <sakshisingh1809@gmail.com>
2023-01-11 13:20:14 +0100

    pushing the repo data

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+
+setup(
+    name='VRP',
+    version='0.1dev',
+    packages=['VRP'],
+    long_description=open('README.md').read(),
+)
---
+++ vrp/__init__.py
@@ -0,0 +1,2 @@
+from .vrp_cplex import datasetgeneration
+from .vrp_cplex import cplexmodel
